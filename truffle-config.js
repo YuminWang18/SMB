02f732141bec1d238689d09255b042a0ecac0106
Yumin Wang <74644116+YuminWang18@users.noreply.github.com>
2023-11-03 15:25:50 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -43,14 +43,14 @@ module.exports = {
   networks: {
     
     development: {
-      host: "127.0.0.1",     // Localhost (default: none)
-      port: 7545,            // Standard Ethereum port (default: none)
-      network_id: "*",       // Any network (default: none)
+      host: "127.0.0.1",     // Localhost 
+      port: 7545,            // Standard Ethereum port 
+      network_id: "*",       // Any network
     },
     compilers: {
     solc: {
-      version: "0.5.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      version: "0.5.1",    // truffle's version
+      docker: true,       
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: false,
